2b7a251ffeca731d1645b93cbfba948ee9fd25e7
Dmitriy Shirochenko <d.shirochenko@criteo.com>
2024-01-30 13:16:53 +0100

    code optimization

---
+++ src/config/config.py
@@ -0,0 +1,22 @@
+import os
+
+from pydantic import Field
+from pydantic_settings import BaseSettings
+
+
+# Корень проекта
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+env_file = ".env"
+ENV_FILE_PATH = os.path.join(BASE_DIR, env_file)
+
+
+class Settings(BaseSettings):
+    app_debug_level: str = Field("INFO", env="APP_DEBUG_LEVEL")
+    base_dir: str = Field(BASE_DIR)
+
+    class Config:
+        env_file = ENV_FILE_PATH
+
+
+settings = Settings()
---
+++ src/config/logger.py
@@ -0,0 +1,37 @@
+from src.config.config import settings
+
+LOG_FORMAT = "%(asctime)s - %(name)s -  - [%(filename)s:%(lineno)d] - %(levelname)s - %(message)s"
+LOG_DEFAULT_HANDLERS = ["console"]  # Added "file" here
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {"format": LOG_FORMAT},
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        },
+        "file": {  # New File Handler
+            "level": "INFO",
+            "class": "logging.FileHandler",
+            "formatter": "verbose",
+            "filename": "app.log",  # Specify your log file name here
+            "mode": "w",  # Append mode
+        },
+    },
+    "loggers": {
+        "": {
+            "handlers": LOG_DEFAULT_HANDLERS,
+            "level": settings.app_debug_level,
+        },
+    },
+    "root": {
+        "level": "INFO",
+        "formatter": "verbose",
+        "handlers": LOG_DEFAULT_HANDLERS,
+    },
+}
